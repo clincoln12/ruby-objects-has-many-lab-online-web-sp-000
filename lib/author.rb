986394080266aa4fd1a8dc1b00057904addf90e3
Chris Lincoln <chris.lincoln12@gmail.com>
2019-07-06 13:06:58 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,15 +1,18 @@
 class Author
-  attr_accessor :name, :posts
+  attr_accessor :name, :posts_arr
   
   def initialize(name)
     @name = name
-    @posts = []
+    @posts_arr = []
   end
   
-  def add_post(post)
+  def add_post(posts)
   end
   
   def add_post_by_title(post_title)
+  end
+  
+  def self.post_count
   end
   
 end
